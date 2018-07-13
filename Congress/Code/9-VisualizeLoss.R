261447156ffd7bb71b32a7eaba7f434b75185104
prodriguezsosa <pedro.rodriguezsosa@gmail.com>
2018-07-13 10:29:24 -0400

    loss visualization

--- Congress/Code/9-VisualizeLoss.R
+++ Congress/Code/9-VisualizeLoss.R
@@ -1,8 +1,17 @@
+rm(list = ls())
 
+
+# ================================
+# define paths
+# ================================
 
 in_path <- "/Users/pedrorodriguez/Dropbox/GitHub/Partisan-Representations/Congress/Post-Estimation/Loss/"
 
-GROUPS <- list("FF", "MF", "MM", "FM")
+
+# ================================
+# define groups
+# ================================
+GROUPS <- list("FF", "MF", "MM", "FM", "RR", "RD", "DD", "DR")
 
 loss_list <- lapply(GROUPS, function(i) readRDS(paste0(in_path, i,"_loss_history.rds")))
 
